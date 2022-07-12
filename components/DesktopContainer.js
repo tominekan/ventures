09e9c35c84f91af926c68ede4b0232abad9162f6
Tomi Adenekan <tominekan@outlook.com>
2022-07-12 16:07:57 -0700

    Adding Intro Containers

---
+++ components/DesktopContainer.js
@@ -0,0 +1,12 @@
+import IntroContainer from "./IntroContainer";
+
+
+const DesktopContainer = () => {
+    return (
+        <>
+            <IntroContainer />
+        </>
+    )
+}
+
+export default DesktopContainer;
---
+++ components/IntroContainer.js
@@ -0,0 +1,18 @@
+const IntroContainer = () => {
+    return (
+        <div className="intro_container">
+            <div className="intro_descrption">
+                <div className="intro_text">
+                    <h1>
+                        Check some of my
+                        <br/>
+                        <b>Mini Ventures</b>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default IntroContainer;
