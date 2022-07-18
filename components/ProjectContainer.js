871560eb8e04783ee4a01f0f4d81373774895db4
Tomi Adenekan <tominekan@outlook.com>
2022-07-17 21:16:45 -0700

    Finish dat website boiii

--- components/ProjectContainer.js
+++ components/ProjectContainer.js
@@ -1,35 +1,65 @@
 import ProjectItem from "./ProjectItem";
 import style from "../styles/components/ProjectContainer.module.css";
 
-// Images
+// IMPORT THEM IMAGES BABYYYYY
+
+// Old Blog
+import oldHome from "../public/OLD_BLAHG/MUSINGS_PAGE.jpeg";
+import oldAbout from "../public/OLD_BLAHG/musings_about_page.png";
+import oldPost from "../public/OLD_BLAHG/post_page.png";
 import defaultImage from "../public/default_image.png";
-import blogPhoto from "../public/BLOG_PHOTO_OF_ME.png";
-import background from "../public/Background.png";
+
+// Porfolio Website
+import portfolioHome from "../public/PORTFOLIO_WEBSITE/intro_page.png";
+import portfolioAbout from "../public/PORTFOLIO_WEBSITE/about_page.png";
+import portfolioSport from "../public/PORTFOLIO_WEBSITE/sport_page.png";
+import portfolioContact from "../public/PORTFOLIO_WEBSITE/contact_page.png";
+
+// Revamped Blog
+import newHome from "../public/REVAMPED_BLAHG/desktop_home.png";
+import newRead from "../public/REVAMPED_BLAHG/desktop_read.png";
+import newArchive from "../public/REVAMPED_BLAHG/desktop_archive.png";
+import newAbout from "../public/REVAMPED_BLAHG/desktop_about.png";
+import newContact from "../public/REVAMPED_BLAHG/desktop_contact.png";
+
+// This site
+import thisHome from "../public/VENTURE_SITE/Ventures.png";
+import thisProjects from "../public/VENTURE_SITE/venture_projects.png";
+import thisAbout from "../public/VENTURE_SITE/desktop_about.png";
+import thisMotive from "../public/VENTURE_SITE/venture_motive.png";
 
 export const projects = [
     {
-        id: "wild_tale",
-        image: blogPhoto,
-        title: "A Wild Tale", 
-        content: "I like tests, give me a little bit of a chance so that i can test you too.",
-        moreImages: [defaultImage, background, blogPhoto],
+        id: "old_blahg",
+        image: oldHome,
+        title: "Tomi's Musings (Website)", 
+        content: "This website was made in order to share my thoughts, unfortunately, due to the amount of work I had to do updating the blog every time, it was quickly abandoned.",
+        moreImages: [oldHome, oldAbout, oldPost],
     },
 
     {
-        id: "wilder_tale",
-        image: defaultImage,
-        title: "A Wilder Tale",
-        content: "I also like tests, give me a little bit of a chance so that i can test you too.",
-        moreImages: [defaultImage, defaultImage, defaultImage, defaultImage],
+        id: "portfolio_website",
+        image: portfolioHome,
+        title: "Porfolio Website",
+        content: "The portfolio website was quite interesting to build because it provided a simple design that I was able to adapt for mobile screens by plugging it full of media queries",
+        moreImages: [portfolioHome, portfolioAbout, portfolioSport, portfolioContact],
     },
 
     {
-        id: "wildest_tale",
-        image: defaultImage,
-        title: "A Wildest Tale",
-        content: "JM I'm a onefour veteran, boss of my hood cause I back my section, when I met the streets and slapped that thing",
-        moreImages: [defaultImage, defaultImage],
-    }
+        id: "revamped_blahg",
+        image: newHome,
+        title: "Revamped Blog (Design)",
+        content: "This is still a design, but I plan on building it out soon enough. I hope to build each section with custom made software (except for security bits).",
+        moreImages: [newHome, newRead, newArchive, newAbout, newContact],
+    }, 
+
+    {
+        id: "venture_site",
+        image: thisHome,
+        title: "This Site (Ventures)",
+        content: "Obviously, I'm not skipping out on this site, it's my first site built with react and next.js. And howver frustrating it was to build it, I'm quite proud of it.",
+        moreImages: [thisHome, thisProjects, thisAbout, thisMotive]
+    },
 ]
 
 const ProjectContainer = (props) => {
